f7bcb86b132ab0101011dd3e63e610502c60fe4d
rickyc <rickyc.us@gmail.com>
2013-05-22 09:00:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nimbus
+
+### Description
+Static file cloud hosting… drag and drop a zip file and it'll extract the contents and upload to s3.
+
+### Dependencies
+See `package.json`
+
+### Configuration
+`S3_ACCESS_KEY_ID`
+
+`S3_SECRET_ACCESS_KEY`
+
+`S3_BUCKET_NAME`
---
+++ app.js
@@ -0,0 +1,124 @@
+//////////////////////////////
+// Module dependencies.
+//////////////////////////////
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , upload = require('jquery-file-upload-middleware')
+  , uuid = require('node-uuid');
+
+var app = express();
+
+
+//////////////////////////////
+// Uploader
+//////////////////////////////
+upload.configure({
+  uploadDir: __dirname + '/public/uploads',
+  uploadUrl: '/uploads',
+});
+
+upload.on('begin', function(fileInfo) {
+  console.log(fileInfo);
+});
+
+upload.on('end', function(fileInfo) {
+  console.log(fileInfo);
+
+  if (fileInfo.type == 'application/zip') {
+    var AdmZip = require('adm-zip');
+
+    var session = fileInfo.url.match(/\/uploads\/(.*)\//)[1]
+
+    var zip = new AdmZip(__dirname + '/public/uploads/' + session + '/' + fileInfo.name);
+    var zipEntries = zip.getEntries();
+
+
+    var client = require('knox').createClient({
+      key: process.env.S3_ACCESS_KEY_ID,
+      secret: process.env.S3_SECRET_ACCESS_KEY,
+      bucket: process.env.S3_BUCKET_NAME
+    });
+
+    var id = uuid.v1();
+
+    zipEntries.forEach(function(zipEntry) {
+      console.log(zipEntry.toString());
+      var decompressedData = zip.readFile(zipEntry);
+      console.log(decompressedData);
+      var headers = { 'Content-Type': 'text/plain' }
+      console.log(zipEntry.entryName);
+      client.putBuffer(decompressedData, "/" + id + "/" + zipEntry.entryName.replace(/ /g, '%20'), headers, function(err, res) { });
+    });
+  }
+});
+
+
+//////////////////////////////
+// Environment Configuration
+//////////////////////////////
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use('/upload', function (req, res, next) {
+    upload.fileHandler({
+      uploadDir: function () {
+        return __dirname + '/public/uploads/' + req.sessionID
+      },
+      uploadUrl: function () {
+        return '/uploads/' + req.sessionID
+      }
+    })(req, res, next);
+  });
+
+  app.use('/list', function (req, res, next) {
+    upload.fileManager({
+      uploadDir: function () {
+        return __dirname + '/public/uploads/' + req.sessionID
+      },
+      uploadUrl: function () {
+        return '/uploads/' + req.sessionID
+      }
+    }).getFiles(function (files) {
+      res.json(files);
+    });
+  });
+});
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+//////////////////////////////
+// Routes
+//////////////////////////////
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+
+//////////////////////////////
+// Application Start
+//////////////////////////////
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
